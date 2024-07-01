d47af3e42652f59027aa873cfdeae6ec296aaaf4
Divyanshu <divyanshuiit02@gmail.com>
2024-07-01 15:28:58 +0530

    setting media directory

--- foodie/urls.py
+++ foodie/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from receipe.views import *
 
 urlpatterns = [
@@ -27,3 +30,7 @@ urlpatterns = [
     path('logout/', login_page, name='update_receipe'),
     path('signup/', signup_page, name='update_receipe'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
